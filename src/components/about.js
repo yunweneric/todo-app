0497a670556de26515e2979262105cb40c3f81ca
yunwen <yunweneric@gmail.com>
2021-01-28 12:40:13 +0100

    working with react routing part2

--- src/components/about.js
+++ src/components/about.js
@@ -1,14 +1,9 @@
 let About = () => {
   return (
-    <div>
-        <ul className="collection with-header">
-        <li className="collection-header"><h4>First Names</h4></li>
-        <li className="collection-item">Alvin</li>
-        <li className="collection-item">Alvin</li>
-        <li className="collection-item">Alvin</li>
-        <li className="collection-item">Alvin</li>
-      </ul>
-    </div>
+   <div className="center">
+     <h1>About Page</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea placeat iure ducimus ullam? Quae quidem doloremque praesentium in, quisquam esse doloribus officiis cupiditate, id ipsam ducimus commodi dolore voluptatum vitae.</p>
+   </div>
   );
 };
 export default About;
--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,8 @@
 let Home = () => {
     return (
       <div className = "center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum velit voluptates similique labore, blanditiis aspernatur eaque non dolorum quos excepturi dolores iusto nostrum quam numquam est enim ea. Aperiam, et? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsum mollitia odio debitis eveniet sunt numquam laborum temporibus magnam, consequatur tenetur deserunt eius ut quo libero saepe repellat! Ducimus, sed.
+      <h1>Home Page</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla nesciunt facere deserunt dolorum nostrum! Ipsa dolorem labore blanditiis delectus qui nostrum a, aperiam, fuga dolorum, facilis maiores quibusdam cum. Voluptatum!</p>
       </div>
     );
   };
