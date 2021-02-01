0777d53474d4c8da144f49ac4251fc786e6fc960
yunweneric <yunweneric@gmail.com>
2021-02-01 18:00:51 +0100

    added axios

--- src/components/home.js
+++ src/components/home.js
@@ -1,17 +1,45 @@
-let Home = (props) => {
-  // setInterval(() => {
-  //   props.history.push("/about");
-  // }, 5000);
-  return (
-    <div className="center">
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla nesciunt
-        facere deserunt dolorum nostrum! Ipsa dolorem labore blanditiis delectus
-        qui nostrum a, aperiam, fuga dolorum, facilis maiores quibusdam cum.
-        Voluptatum!
-      </p>
-    </div>
-  );
-};
-export default Home;
+import axios from "axios";
+import { Component } from "react";
+import colors from "../hoc/colors";
+
+class Home extends Component {
+  state = {
+    posts: [],
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      this.setState({
+        posts: res.data.slice(0, 10),
+      });
+    });
+  }
+  render() {
+    let { posts } = this.state;
+    let postlist = posts.length ? (
+      posts.map(post =>{
+        return <div className="center post card" key={post.id}>
+          <div className=" card-content">
+            <div className="card-title">
+              <h5>{post.title}</h5>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      })
+    ) : (
+      <div className='center'>
+        <p>No Post found!</p>
+      </div>
+    )
+    return (
+      <div className="center">
+        <h1>Home Page</h1>
+        <p className = 'container'>
+          {postlist}
+        </p>
+        {/* <p>{posts}</p> */}
+      </div>
+    );
+  }
+}
+export default colors(Home);
